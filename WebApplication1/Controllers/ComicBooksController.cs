6401090c83d40867e1e3806d4c8ac069964f19bf
dontflagme <ubermunchpunch@gmail.com>
2019-01-12 10:50:31 -0800

    Added detail action method

--- WebApplication1/Controllers/ComicBooksController.cs
+++ WebApplication1/Controllers/ComicBooksController.cs
@@ -8,6 +8,9 @@ namespace WebApplication1.Controllers
 {
     public class ComicBooksController : Controller
     {
-
+        public String Detail()
+        {
+            return "Hello from ComicBook Controller";
+        }
     }
 }
