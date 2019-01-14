2add85e09561362202b08892fe02e41171c512f2
dontflagme <ubermunchpunch@gmail.com>
2019-01-14 15:57:51 -0800

    added comicbook repository

--- WebApplication1/Controllers/ComicBooksController.cs
+++ WebApplication1/Controllers/ComicBooksController.cs
@@ -11,23 +11,10 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Detail()
         {
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist () {Name = "Script", Role = "Dan Slott"},
-                    new Artist () {Name = "Pencils", Role = "Humberto Ramos"},
-                    new Artist () {Name = "Inks", Role = "Victor Olazaba"},
-                    new Artist () {Name = "Colors", Role = "Edgar Delgad"},
-                    new Artist () {Name = "Letters", Role = "Chris Eliopoulos"},
-                }
-            };
+
 
            
-            return View(comicBook);
+            return View();
            
 
 
