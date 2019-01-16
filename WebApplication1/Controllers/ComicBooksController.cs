9a3f64357506856fd2a25f1ee95d1aaa7959fb6f
dontflagme <ubermunchpunch@gmail.com>
2019-01-15 21:18:22 -0800

    Updated comic books controller to use repository

--- WebApplication1/Controllers/ComicBooksController.cs
+++ WebApplication1/Controllers/ComicBooksController.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Data;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
+        private ComicBookRepository _comicbookRepository = null;
+
+        public ComicBooksController()
         {
-
-
+            _comicbookRepository = new ComicBookRepository();
+        }
+        public ActionResult Detail(int? id)
+        {
+            if(id == null)
+            {
+                return HttpNotFound();
+            }
+            var comicBook = _comicbookRepository.GetComicBook(id.Value);
            
-            return View();
+            return View(comicBook);
            
 
 
